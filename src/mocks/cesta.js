a2ee5d726cde2c91db1feca5c7ec982a44e5fbac
florianoborges <114959608+florianoborges@users.noreply.github.com>
2022-11-26 22:26:23 -0300

    Add files via upload

---
+++ src/mocks/cesta.js
@@ -0,0 +1,47 @@
+import logo from '../../assets/logo.png'
+import tomate from '../../assets/frutas/Tomate.png'
+import brocolis from '../../assets/frutas/Brócolis.png'
+import batata from '../../assets/frutas/Batata.png'
+import pepino from '../../assets/frutas/Pepino.png'
+import abobora from '../../assets/frutas/Abóbora.png'
+
+const cesta = {
+  topo: {
+    titulo: 'Detalhe da cesta',
+  },
+  detalhes: {
+    nome: 'Cesta de Verduras',
+    logoFazenda: logo,
+    nomeFazenda: 'Fazenda Rancho Fundo',
+    descricao: 'Uma cestão por um preção',
+    preco: 'R$ 40,00',
+  },
+
+  itens: {
+    titulo: 'Itens da cesta',
+    lista: [
+      {
+        nome: 'Tomate',
+        imagem: tomate,
+      },
+      {
+        nome: 'Brócolis',
+        imagem: brocolis,
+      },
+      {
+        nome: 'Batata',
+        imagem: batata,
+      },
+      {
+        nome: 'Pepino',
+        imagem: pepino,
+      },
+      {
+        nome: 'Abóbora',
+        imagem: abobora,
+      }
+    ]
+  }
+}
+
+export default cesta;
